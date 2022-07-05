08242515835dad280b3142c51ddbb63f1fda7c54
Cinkowyy <90154960+Cinkowyy@users.noreply.github.com>
2022-07-05 11:01:23 +0200

    Project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.scss";
+import Landing from "./pages/landing/Landing";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Landing />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/pages/landing/Landing.tsx
@@ -0,0 +1,7 @@
+import Logo from "img/Logo.svg";
+
+const Landing = () => {
+  return <img src={Logo} alt="Łappka" />;
+};
+
+export default Landing;

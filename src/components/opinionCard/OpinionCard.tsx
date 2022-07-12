e0edd0eade728d9cd5f8967ff271ec86dd3551fb
Cinkowyy <90154960+Cinkowyy@users.noreply.github.com>
2022-07-12 13:31:24 +0200

    refactor: change slider logic

--- src/components/opinionCard/OpinionCard.tsx
+++ src/components/opinionCard/OpinionCard.tsx
@@ -1,15 +1,28 @@
 import styles from "./OpinionCard.module.scss";
 import quote from "img/quote.svg";
+import React from "react";
 
 interface IOpinionCard {
   author: string;
   age: string;
   content: string;
+  order: number;
 }
 
-const OpinionCard: React.FC<IOpinionCard> = ({ content, author, age }) => {
+const OpinionCard: React.FC<IOpinionCard> = ({
+  content,
+  author,
+  age,
+  order,
+}) => {
   return (
-    <div className={styles.card}>
+    <div
+      className={`${styles.card} ${order === 0 ? styles.active : ""} ${
+        order === -1 ? styles.second : ""
+      } ${order === -2 ? styles.third : ""} ${
+        order > 0 ? styles.prevCard : ""
+      } ${order < -2 ? styles.nextCard : ""}`}
+    >
       <div className={styles.opinion}>
         <img src={quote} alt="" />
         <p className={`grey_paragraph ${styles.content}`}>{content}</p>
--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -27,48 +27,53 @@ const opinionsData: IOpinionCard[] = [
     author: "Joanna",
     age: "23 lata",
   },
+  {
+    content:
+      "Dzięki Łappce bardzo szybko odnalazłam moją ukochaną Calineczkę. Uciekła mi z domu nad ranem i nie mogłam jej dogonić. Zrozpaczona szybko udostępniłam wiadomość o tym w Łappce. Już wieczorem odezwał się do mnie inny użytkownik, mówiąc, że Calineczka jest u niego w ogrodzie. Jakieś 15 minut później już jechałyśmy razem do domu. Cudowna aplikacja!",
+    author: "Malwina",
+    age: "32 lata",
+  },
+  {
+    content:
+      "Już do jakiegoś czasu myślałam o adopcji pieska. Niestety nigdy nie miałam czasu, żeby pojechać do schroniska i znaleźć mojego nowego ukochanego 😊 dzięki Łappce wybrałam Diego, który znajdował się w schronisku blisko mnie. Wystarczyło założenie konta i kilka kliknięć. Teraz jesteśmy z Diegiem w domu i za nic nie zrozumiem dlaczego ten slodziak był w schronisku. Jeśli ktoś myśli o adopcji gorąco polecam Łappkę!",
+    author: "Kunegunda",
+    age: "19 lat",
+  },
+  {
+    content:
+      "Dla mnie to fantastyczna aplikacja, bo znalazłam tam moją Jessy. To ok 8 miesięczna kicia, która przejechała się pod maską samochodu i była poparzona. Była też mocno wychudzona. Kiedy kicia trochę wydobrzała, zabrałam ją do domu. I mam cudowną, mruczącą przyjaciółkę 😊",
+    author: "Joanna",
+    age: "23 lata",
+  },
 ];
 
-const dataLength: number = opinionsData.length;
-let currentCard: number = 1;
-
-console.log("I rerendered");
-
 const Slider = () => {
-  const [opinions, setOpinions] = useState<IOpinionCard[]>(opinionsData);
+  const [currentCard, setCurrentCard] = useState(0);
+  const dataLength = opinionsData.length;
 
   const handleNextCard = () => {
-    if (currentCard >= dataLength) return;
+    if (currentCard >= dataLength - 1) return;
 
-    currentCard += 1;
-    const newOpinions = opinionsData.filter((opinion, index) => {
-      return index >= currentCard - 1;
-    });
-
-    setOpinions(newOpinions);
+    setCurrentCard((prevCard) => prevCard + 1);
   };
 
   const handlePrevCard = () => {
-    if (currentCard <= 1) return;
+    if (currentCard <= 0) return;
 
-    currentCard -= 1;
-    const newOpinions = opinionsData.filter((opinion, index) => {
-      return index >= currentCard - 1;
-    });
-
-    setOpinions(newOpinions);
+    setCurrentCard((prevCard) => prevCard - 1);
   };
 
   return (
     <div className={styles.slider}>
       <div className={styles.opinions_container}>
-        {opinions.map((opinion, index) => {
+        {opinionsData.map((opinion, index) => {
           return (
             <OpinionCard
               key={index}
               content={opinion.content}
               author={opinion.author}
               age={opinion.age}
+              order={currentCard - index}
             />
           );
         })}
@@ -77,14 +82,14 @@ const Slider = () => {
         <button onClick={() => handlePrevCard()}>
           <div
             className={`${styles.arrow} ${styles.arrow_left} ${
-              currentCard > 1 ? styles.active_arrow : null
+              currentCard > 0 ? styles.active_arrow : null
             }`}
           ></div>
         </button>
         <button onClick={() => handleNextCard()}>
           <div
             className={`${styles.arrow} ${styles.arrow_right} ${
-              currentCard < dataLength ? styles.active_arrow : null
+              currentCard < dataLength - 1 ? styles.active_arrow : null
             }`}
           ></div>
         </button>
